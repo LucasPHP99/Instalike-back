966cba8599da3adc915f73e0fa58e9537556e614
LucasPHP99 <lucasphelipe99@gmail.com>
2024-11-22 20:41:00 +0000

    Instalike-back

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import routes from "./src/config/routes/postsRoutes.js";
+
+
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
+
+const app = express();
+app.use(express.static("uploads"))
+routes(app)
+
+app.listen(3000, () => {
+    console.log("Servidor Escutando....");
+});
+
+
+
+
+
